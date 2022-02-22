7474c865586b5f2e86699355de119248acaba936
LTLA <infinite.monkeys.with.keyboards@gmail.com>
2022-02-21 20:14:13 -0800

    Added some test datasets.

---
+++ testing/human_download.sh
@@ -0,0 +1,5 @@
+wget https://github.com/clusterfork/singlepp-references/releases/download/hs-latest/BlueprintEncode_genes.csv.gz -O human/BlueprintEncode_genes.csv.gz
+wget https://github.com/clusterfork/singlepp-references/releases/download/hs-latest/BlueprintEncode_labels_fine.csv.gz -O human/BlueprintEncode_labels_fine.csv.gz
+wget https://github.com/clusterfork/singlepp-references/releases/download/hs-latest/BlueprintEncode_label_names_fine.csv.gz -O human/BlueprintEncode_label_names_fine.csv.gz
+wget https://github.com/clusterfork/singlepp-references/releases/download/hs-latest/BlueprintEncode_markers_fine.gmt.gz -O human/BlueprintEncode_markers_fine.gmt.gz
+wget https://github.com/clusterfork/singlepp-references/releases/download/hs-latest/BlueprintEncode_matrix.csv.gz -O human/BlueprintEncode_matrix.csv.gz
---
+++ testing/mouse_download.sh
@@ -0,0 +1,5 @@
+wget https://github.com/clusterfork/singlepp-references/releases/download/mm-latest/ImmGen_genes.csv.gz -O mouse/ImmGen_genes.csv.gz
+wget https://github.com/clusterfork/singlepp-references/releases/download/mm-latest/ImmGen_labels_fine.csv.gz -O mouse/ImmGen_labels_fine.csv.gz
+wget https://github.com/clusterfork/singlepp-references/releases/download/mm-latest/ImmGen_label_names_fine.csv.gz -O mouse/ImmGen_label_names_fine.csv.gz
+wget https://github.com/clusterfork/singlepp-references/releases/download/mm-latest/ImmGen_markers_fine.gmt.gz -O mouse/ImmGen_markers_fine.gmt.gz
+wget https://github.com/clusterfork/singlepp-references/releases/download/mm-latest/ImmGen_matrix.csv.gz -O mouse/ImmGen_matrix.csv.gz
