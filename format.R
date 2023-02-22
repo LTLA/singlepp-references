796c0c279548744952a06c83a1e1381486a69326
Aaron Lun <lun.aaron@gene.com>
2023-02-22 10:23:23 -0800

    Updated the EnsDb versions for mapping features, added Entrez IDs.

--- format.R
+++ format.R
@@ -2,7 +2,7 @@ library(celldex)
 library(SingleR)
 library(matrixStats)
 
-formatter <- function(x, prefix, dir) {
+formatter <- function(x, prefix, dir = "v1.0.0") {
     dir.create(dir, showWarnings=FALSE)
 
     mat <- assay(x, "logcounts")
@@ -11,9 +11,10 @@ formatter <- function(x, prefix, dir) {
     write.table(file=mhandle, rmat, sep=",", col.names=FALSE, row.names=FALSE)
 
     ghandle <- gzfile(file.path(dir, paste0(prefix, "_genes.csv.gz")))
-    fdf <- rowData(x)[,c("ensembl", "symbol"),drop=FALSE]
+    fdf <- rowData(x)[,c("ensembl", "symbol", "entrez"),drop=FALSE]
     fdf$ensembl[is.na(fdf$ensembl)] <- ""
     fdf$symbol[is.na(fdf$symbol)] <- ""
+    fdf$entrez[is.na(fdf$entrez)] <- ""
     write.table(fdf, file=ghandle, sep=",", row.names=FALSE, col.names=FALSE, quote=FALSE)
 
     for (lab in colnames(colData(x))) {
@@ -47,10 +48,10 @@ ahub <- AnnotationHub()
 
 for (species in c("hs", "mm")) {
     if (species == "hs") {
-        ens <- ahub[["AH73881"]]
+        ens <- ahub[["AH109336"]] # Ensembl 108.
         datasets <- c("BlueprintEncode", "HumanPrimaryCellAtlas", "MonacoImmune", "DatabaseImmuneCellExpression", "NovershternHematopoietic", "MonacoImmune")
     } else {
-        ens <- ahub[["AH73905"]]
+        ens <- ahub[["AH109367"]] # Ensembl 108.
         datasets <- c("ImmGen", "MouseRNAseq")
     }
 
@@ -59,12 +60,14 @@ for (species in c("hs", "mm")) {
 
         if (!all(grepl("^ENS", rownames(X)))) {
             rowData(X)$ensembl <- mapIds(ens, keys = rownames(X), keytype = "SYMBOL", column = "GENEID")
+            rowData(X)$entrez <- mapIds(ens, keys = rownames(X), keytype = "SYMBOL", column = "ENTREZID")
             rowData(X)$symbol <- rownames(X)
         } else {
             rowData(X)$ensembl <- rownames(X)
+            rowData(X)$entrez <- mapIds(ens, keys = rownames(X), keytype = "GENID", column = "ENTREZID")
             rowData(X)$symbol <- mapIds(ens.hs, keys = rownames(X), keytype = "GENEID", column = "SYMBOL")
         }
 
-        formatter(X, ref, dir=species)
+        formatter(X, ref)
     }
 }
